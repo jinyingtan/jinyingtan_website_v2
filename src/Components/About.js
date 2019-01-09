de852aba11d31a7885fd4c23021d33aa43069dbe
Jin Ying <developer.tjy@gmail.com>
2019-01-09 15:59:52 +0800

    About.js: Update description and Add Tools

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FaHtml5, FaCss3, FaJsSquare, FaPython, FaJava } from 'react-icons/fa'
 
 import '../css/about.css'
 
@@ -14,6 +15,22 @@ class About extends Component {
   }
 
   render() {
+    const toolbox = [<FaHtml5/>, <FaCss3/>, <FaJsSquare/>, <FaJava/>, <FaPython/>];
+
+    const MyToolBox = () => {
+      let toolbox_div = toolbox.map((e, index) => {
+        return (
+          <span className="tool" key={index}>{e}</span>
+        )
+      })
+      
+      return (
+        <div className="toolbox-details">
+          {toolbox_div}
+        </div>
+      )
+    }
+
     return (
       <section id="about" ref={this.state.refProps}>
         <div id="about-header" className="text-center">
@@ -28,16 +45,24 @@ class About extends Component {
         <div id="about-content" className="mt-4">
           <article className="description">
             Every website has a story of their own. They tell it through their own features and aesthetic design presented to its users. A user who enters the website look through the story of the website and decides whether the story has the potential to add value to them.
-                        <br /><br />
+            <br /><br />
             My goal as a web developer is to bring out the aesthetic design of any website and highlight the features of the website to its users.
-                        <br /><br />
+            <br /><br />
             I have graduated from Singapore Polytechnic with a Diploma in Computer Engineering (2012 – 2015). It is through these 3 years in my polytechnic education that I found my love for web developer. What I like about web development is that I feel that it is amazing that having just a simple website it can convey its message and provide values through to its users throughout the world.
-                        <br /><br />
-            I’m currently pursuing a degree in Computer Science at the National University of Singapore. I’m looking to hone my skills as a programmer there and to discover different areas of computing. During my free time, I also look to improve myself as I learn new programming languages and continue to build my own personal projects.
-                        <br /><br />
+            <br /><br />
+            I’m currently pursuing a Bachelor in Computer Science at the National University of Singapore. I am currently a Year 2 student there and is specializing in Software Engineering and Database Management.
+            During my free time, I continue to learn more about software development and work on my own pet projects.
+            <br /><br />
             <a href="mailto:developer.tjy@gmail.com?subject=Hello%20Jin%20Ying">Chat me up </a> if you would like to talk to me about any areas web development or would like to me to help you convey your story through your website.
-                    </article>
+          </article>
+
+          <div className="my-toolbox">
+            <h4>My Toolbox</h4>
+
+            <MyToolBox />
+          </div>
         </div>
+
       </section>
     )
   }
