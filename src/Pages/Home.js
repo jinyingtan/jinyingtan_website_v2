857731dc7e5f8a63ca23ed1ef520822b42166fb0
Jin Ying <kenneth.jyt@gmail.com>
2020-05-03 22:06:29 +0800

    Update Porfolio Simulation and Format Code

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,56 +7,56 @@ import Portfolio from '../Components/Portfolio';
 import Footer from '../Components/Footer';
 
 class Home extends Component {
-    constructor() {
-        super()
+  constructor() {
+    super()
 
-        const aboutComponent = React.createRef()
-        const portfolioComponent = React.createRef()
-        const introComponent = React.createRef()
+    const aboutComponent = React.createRef()
+    const portfolioComponent = React.createRef()
+    const introComponent = React.createRef()
 
-        this.state = {
-            aboutComponent: aboutComponent,
-            portfolioComponent: portfolioComponent,
-            introComponent: introComponent,
-            refs: { aboutComponent, portfolioComponent }
-        }
+    this.state = {
+      aboutComponent: aboutComponent,
+      portfolioComponent: portfolioComponent,
+      introComponent: introComponent,
+      refs: { aboutComponent, portfolioComponent }
     }
+  }
 
-    componentDidMount() {
-        let component = window.location.hash;
+  componentDidMount() {
+    let component = window.location.hash;
 
-        switch (component) {
-            case "#about":
-                this.scrollToDiv(this.state.aboutComponent)
-                break;
-            case "#portfolio":
-                this.scrollToDiv(this.state.portfolioComponent)
-                break;
-            case "":
-                break;
-            default:
-                this.scrollToDiv(this.state.introComponent)
-        }
+    switch (component) {
+      case "#about":
+        this.scrollToDiv(this.state.aboutComponent)
+        break;
+      case "#portfolio":
+        this.scrollToDiv(this.state.portfolioComponent)
+        break;
+      case "":
+        break;
+      default:
+        this.scrollToDiv(this.state.introComponent)
     }
+  }
 
-    scrollToDiv = (ref) => {
-        window.scrollTo({
-            top: ref.current.offsetTop,
-            behavior: "smooth"
-        })
-    }
+  scrollToDiv = (ref) => {
+    window.scrollTo({
+      top: ref.current.offsetTop,
+      behavior: "smooth"
+    })
+  }
 
-    render() {
-        return (
-            <React.Fragment>
-                <Header isHome={true} contentRefs={this.state.refs} />
-                <Intro refProps={this.state.introComponent} portfolioRef={this.state.portfolioComponent} />
-                <About refProps={this.state.aboutComponent} />
-                <Portfolio refProps={this.state.portfolioComponent} />
-                <Footer />
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Header isHome={true} contentRefs={this.state.refs} />
+        <Intro refProps={this.state.introComponent} portfolioRef={this.state.portfolioComponent} />
+        <About refProps={this.state.aboutComponent} />
+        <Portfolio refProps={this.state.portfolioComponent} />
+        <Footer />
+      </React.Fragment>
+    )
+  }
 }
 
 export default Home;
